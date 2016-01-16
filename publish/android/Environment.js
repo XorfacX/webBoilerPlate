0bb3a2d26334459596cdf0ed9eb7450e7ce0b449
XorfacX <xorfacx@fairydwarves.com>
2016-01-16 20:49:11 +0100

    android event test code

--- publish/android/Environment.js
+++ publish/android/Environment.js
@@ -5,7 +5,18 @@ setEnvironment = function () {
         var android_env = declare(_AppEnvCreation, {
             constructor: function () {
                 //TOSET: ANDROID specific env code goes here
-
+                document.addEventListener('deviceready', function () {
+                    console.log("cordova loaded ok");
+                    alert("cordova loaded ok");
+                    document.addEventListener('pause', function () {
+                        console.log("pause event detected");
+                        alert("pause event detected");
+                    }, false);
+                    document.addEventListener('resume', function () {
+                        console.log("resume event detected");
+                        alert("resume event detected");
+                    }, false);
+                }, false);
             } //constructor
         });  //declare
         android_env();
